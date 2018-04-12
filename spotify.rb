8ad95e75667a006df6b77ed0c8f6fd1b16d3b427
shwetzpatil <shwetzpatil@gmail.com>
2018-04-12 16:37:08 +0100

    finished final view

--- spotify.rb
+++ spotify.rb
@@ -21,44 +21,6 @@ get ("/event") do
 end
 
 get ("/music") do
-artistlist = RSpotify::Artist.search('Arctic', limit:3)
-@top_artists = artistlist
-@firstartist = artistlist[1]
-@firstartist1 = artistlist[2]
-@firstartist2 = artistlist[3]
-
-artists = RSpotify::Artist.search('Pitbull')
-arctic_monkeys = artists.first
-print arctic_monkeys.genres 
-@album = arctic_monkeys.genres 
-
-# testing
-artist = RSpotify::Artist.find('0TnOYISbd1XYRBk9myaseg')
-artist.class #=> RSpotify::Artist
-artist_name = artist.name 
-@name = artist_name
-
-link_artist = artist.external_urls
-@link = link_artist['spotify']
-
-images_artist = artist.images.first['url']
-@image = images_artist
-
-album_artist = artist.albums.first
-album_name = album_artist.name
-@album_name = album_name
-
-tracks_artist = album_artist.tracks
-@tracks = tracks_artist
-first_track = tracks_artist.first
-@first_track_url = first_track.preview_url
-@first_track_name = first_track.name
-
-second_track = tracks_artist[2]
-@second_track_url = second_track.preview_url
-@second_track_name = second_track.name
-
-# code required for music page
 @albums_list = RSpotify::Album.new_releases(limit: 10)
 @name_text = @albums_list.first.name
 
@@ -74,7 +36,7 @@ post ("/search") do
   if @dropdownquery === "artist"
     result = RSpotify::Artist.search(inputquery)
     puts result.inspect
-    @result_show = result[2].external_urls['spotify']
+    @result_show = result.first.external_urls['spotify']
   elsif @dropdownquery === "album"
     result = RSpotify::Album.search(inputquery) 
     puts result.inspect
@@ -87,3 +49,4 @@ post ("/search") do
   "hello world"
   erb :search, layout: :layout
 end  
+
